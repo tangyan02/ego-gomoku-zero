e65d6d9a1c6662b5ad19c91d21b23a01a820805f
kaeltang <kaeltang@tencent.com>
2024-03-08 17:49:50 +0800

    update

--- Main.cpp
+++ Main.cpp
@@ -5,7 +5,7 @@
 #include "Console.h"
 #include "Shape.h"
 #include "ShapeTest.h"
-#include "SelfPlayTest.h"
+#include <onnxruntime_cxx_api.h>
 
 using namespace std;
 
@@ -96,9 +96,44 @@ void test() {
     }
 }
 
+void onnxTest() {
+    // 初始化环境
+    Ort::Env env(ORT_LOGGING_LEVEL_WARNING, "ModelInference");
+
+    // 初始化会话选项并添加模型
+    Ort::SessionOptions session_options;
+    session_options.SetIntraOpNumThreads(1);
+
+    const char *model_path = "model/model_latest.onnx";
+
+    Ort::Session session(env, model_path, session_options);
+    // 创建输入张量
+    std::vector<float> input_tensor_values(1 * 18 * 20 * 20); // 填充你的实际数据
+    std::vector<int64_t> input_tensor_shape = {1, 18, 20, 20};
+    auto memory_info = Ort::MemoryInfo::CreateCpu(OrtArenaAllocator, OrtMemTypeDefault);
+    Ort::Value input_tensor = Ort::Value::CreateTensor<float>(memory_info, input_tensor_values.data(), input_tensor_values.size(), input_tensor_shape.data(), input_tensor_shape.size());
+
+    // 设置输入
+    std::vector<const char*> input_node_names = {"input"};
+    std::vector<Ort::Value> input_tensors;
+    input_tensors.push_back(std::move(input_tensor));
+
+    // 设置输出
+    std::vector<const char*> output_node_names = {"output"};
+
+    // 进行推理
+    auto output_tensors = session.Run(Ort::RunOptions{nullptr}, input_node_names.data(), input_tensors.data(), input_tensors.size(), output_node_names.data(), output_node_names.size());
+
+    // 获取输出张量
+    float* floatarr = output_tensors.front().GetTensorMutableData<float>();
+    std::cout << "Output: " << floatarr[0] << std::endl;
+
+}
+
 int main(int argc, char *argv[]) {
-    initShape();
-    selfPlay(argc, argv);
+    onnxTest();
+//    initShape();
+//    selfPlay(argc, argv);
 //    test();
 //    piskvork();
 //    startConsole(true);
