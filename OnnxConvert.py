79d65832ee29e50ffbc3dd689adf04f3de292e23
kaeltang <kaeltang@tencent.com>
2024-03-13 13:02:29 +0800

    update

--- OnnxConvert.py
+++ OnnxConvert.py
@@ -1,4 +1,6 @@
+import onnx
 import torch
+from onnxsim import simplify
 
 import Network
 
@@ -6,13 +8,21 @@ lr = 0.001
 network, optimizer = Network.get_network("cpu", lr)
 network.eval()
 
+path = 'model/model_latest.onnx'
+
 example = torch.randn(22, 20, 20, requires_grad=True)
 print(example)
 
 torch.onnx.export(network,
                   (example),
-                  'model/model_latest.onnx',
+                  path,
                   input_names=['input'],
                   output_names=['value', "act"],
                   opset_version=17,
                   verbose=True)
+
+onnx_model = onnx.load(path)  # load onnx model
+model_simp, check = simplify(onnx_model)
+assert check, "Simplified ONNX model could not be validated"
+onnx.save(model_simp, path + "_simple")
+print('finished exporting onnx')
